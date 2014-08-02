239ac7379e5aa72e45001cc179a25f0611f16c53
amcereijo <amcereijo@gmail.com>
2014-08-02 20:14:01 +0200

    Assigments level 4

---
+++ level4/4.15_Secure_the_bats/cavesofclarity.js
@@ -0,0 +1,15 @@
+var CAVESOFCLARITY = function () {
+          
+  var treasureChests = 3;
+  var bats = 345;
+  return {
+    stalagmites: 3924,
+    stalactites: 4235,
+    SECRET: {
+      openChest: function(){
+        treasureChests--;
+        alert("DA DADADA DAAAAAAA!");
+      }
+    }
+  };
+}();
---
+++ level4/4.17_Bats_all_time/cavesofclarity.js
@@ -0,0 +1,20 @@
+var CAVESOFCLARITY = function () {
+          
+  var treasureChests = 3;
+  var bats = 345;
+
+  return {
+    stalactites: 4235,
+    stalagmites: 3924,
+    SECRET: {
+      openChest: function(){
+        treasureChests--;
+        alert("DA DADADA DAAAAAAA!");
+      }
+    },
+    getBats: function() {
+      return bats;
+    }
+
+  };
+}();
